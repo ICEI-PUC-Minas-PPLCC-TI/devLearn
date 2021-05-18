2daec86c2360a852403d3205e725676e3858c1a7
vfsouza <vini.f.souza@hotmail.com>
2021-05-17 23:35:04 -0300

    Adicionado os arquivos cadastrar-usuario.hmtl e cadastrar-usuarios.js. Atualização do arquivo style.css

---
+++ src/scripts/cadastrar-usuarios.js
@@ -0,0 +1,66 @@
+const LOGIN_URL = "login.html";
+
+var db_usuarios = {};
+
+var usuarioCorrente = {};
+
+function generateUUID() {
+   var d = new Date().getTime();
+   var d2 = (performance && performance.now && (performance.now()*1000)) || 0;
+   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16;
+      if(d > 0){
+         r = (d + r)%16 | 0;
+         d = Math.floor(d/16);
+      } else {
+         r = (d2 + r)%16 | 0;
+         d2 = Math.floor(d2/16);
+      }
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+   });
+}
+const dadosIniciais = {
+   usuarios: [
+      { "id": generateUUID (), "login": "admin", "senha": "123", "nome": "Administrador do Sistema"},
+      { "id": generateUUID (), "login": "user", "senha": "123", "nome": "Usuario Comum"},
+   ]
+};
+
+function initLoginApp () {
+   usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+   if (usuarioCorrenteJSON) {
+      usuarioCorrente = JSON.parse (usuarioCorrenteJSON);
+   }
+    
+   var usuariosJSON = localStorage.getItem('db_usuarios');
+   if (!usuariosJSON) {
+      alert('Dados de usuários não encontrados no localStorage. \n -----> Fazendo carga inicial.');
+
+      db_usuarios = dadosIniciais;
+
+      localStorage.setItem('db_usuarios', JSON.stringify (dadosIniciais));
+   }
+   else  {
+      db_usuarios = JSON.parse(usuariosJSON);    
+   }
+};
+
+function logoutUser () {
+   usuarioCorrente = {};
+   sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+   window.location = LOGIN_URL;
+}
+
+function addUser (nome, login, senha) {
+   let newId = generateUUID ();
+   let usuario = { "id": newId, "login": login, "senha": senha, "nome": nome};
+    
+   db_usuarios.usuarios.push (usuario);
+
+   localStorage.setItem('db_usuarios', JSON.stringify (db_usuarios));
+}
+
+function setUserPass () {
+
+}
+initLoginApp ();

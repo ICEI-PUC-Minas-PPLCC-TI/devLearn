5ecc83951770669e232df65573e51d3c39efe704
Davi P C <80406029+daavipc@users.noreply.github.com>
2021-05-19 21:26:47 -0300

    atualização na table

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -4,3 +4,9 @@ $('#preloader .inner').fadeOut();
 $('#preloader').delay(200).fadeOut('slow');
 $('body').delay(200).css({'overflow': 'visible'});
 })
+
+//table
+$(window).on("load resize ", function() {
+var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+$('.tbl-header').css({'padding-right':scrollWidth});
+}).resize();

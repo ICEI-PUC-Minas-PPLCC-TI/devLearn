10ce5e042dff44a5a0b731e49f0257fa7ec6792c
lfnand0 <lfnando.nando@gmail.com>
2021-05-19 23:37:50 -0300

    Bug fix de pesquisa

--- src/scripts/busca.js
+++ src/scripts/busca.js
@@ -1,11 +1,50 @@
-const busca = document.getElementById("busca-2");
-// let bancoDeDados = [];         - utilizar quando banco de dados de conteúdo estiver disponível
+var db_cursos_iniciais = {
+  data: [
+    {
+      id: 1,
+      titulo: "O guia estelar de HTML",
+      nome: "Rocketseat",
+      idioma: "PT",
+      conteudo: "HTML",
+      tags: "Desenvolvimento WEB, HTML",
+    },
+    {
+      id: 2,
+      titulo: "O guia estelar de CSS",
+      nome: "Rocketseat",
+      idioma: "PT",
+      conteudo: "CSS",
+      tags: "Desenvolvimento WEB, CSS",
+    },
+    {
+      id: 3,
+      titulo: "O guia estelar de Javascript",
+      nome: "Rocketseat",
+      idioma: "PT",
+      conteudo: "Javascript",
+      tags: "Desenvolvimento WEB, Javascript",
+    },
+  ],
+};
 
-busca.addEventListener("keyup", (e) => {
-  const searchString = e.target.value.toLowerCase();
+var db = JSON.parse(localStorage.getItem("db_curso"));
+if (!db) {
+  db = db_cursos_iniciais;
+}
 
-  const filteredContent = bancoDeDados.filter((content) => {
-    return contentName.toLowerCase().includes(searchString);
-  });
-  displayContent(filteredContent);
-});
+function buscarCurso() {
+  const busca = $("#busca-1").val();
+  console.log("DEBUG: FUNCAO buscarCurso: ", busca);
+  let obj = db.data.find((o) => o.conteudo.toLowerCase() == busca.toLowerCase());
+  localStorage.setItem("obj_curso", JSON.stringify(obj));
+  console.log("DEBUG: OBJ ", obj);
+}
+
+// busca.addEventListener("keyup", (e) => {
+//   const searchString = e.target.value.toLowerCase();
+
+//   const filteredContent = bancoDeDados.filter((content) => {
+//     return contentName.toLowerCase().includes(searchString);
+//   });
+//   displayContent(filteredContent);
+// });

118dd30ec8da7b903f327d8f6d81336376be8a64
Davi P C <80406029+daavipc@users.noreply.github.com>
2021-06-02 16:36:46 -0300

    criação aba perfil/att

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -10,3 +10,8 @@ $(window).on("load resize ", function() {
 var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
 $('.tbl-header').css({'padding-right':scrollWidth});
 }).resize();
+
+// tooltip
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
